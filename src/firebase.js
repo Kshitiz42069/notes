51582e0d69a70d4853973d5d7449cf3e799e4d82
KSHITIZ RAI <kshitizroy836@gmail.com>
2023-10-31 23:44:10 +0530

    Firebase add

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyAHoIoPMPo6_RdhUsTUiTS4xIFWWlW74g4",
+    authDomain: "notes-78c0c.firebaseapp.com",
+    projectId: "notes-78c0c",
+    storageBucket: "notes-78c0c.appspot.com",
+    messagingSenderId: "790833890043",
+    appId: "1:790833890043:web:77b0fc53f57e62965be65e",
+    measurementId: "G-NZLZ5QTTED"
+  };
